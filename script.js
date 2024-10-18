03769a31f017f585e8c434e1f62e68ea42990f17
Ambs04 <amberleejones2007@gmail.com>
2024-10-18 17:33:15 +0200

    declare regexp pattern

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+  let input = document.getElementById("petInput").value;
+  // Validation logic goes here
+  let result = ""; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+  // TODO: Write your validation logic here
+  // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+  document.getElementById("result").innerText = result;
 }
 
+//declaration of regular expression
 
+const syntaxPattern = /[pet_]{4}[0-9]{4}[A-Za-z]{3,}/g;
