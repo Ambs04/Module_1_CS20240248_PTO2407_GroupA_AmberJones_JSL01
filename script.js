2f50c06d7a5465af63473ec24adf8bda56e2e2ef
Ambs04 <amberleejones2007@gmail.com>
2024-10-18 17:40:18 +0200

    create function to check the input against the regex syntax by stating input and regex as parameters

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function validateSyntax() {
   let input = document.getElementById("petInput").value;
   // Validation logic goes here
-  let result = ""; // Placeholder for validation result
-
-  // TODO: Write your validation logic here
-  // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-  document.getElementById("result").innerText = result;
+  // Placeholder for validation result
+  let result = "";
 }
 
 //declaration of regular expression
 
 const syntaxPattern = /[pet_]{4}[0-9]{4}[A-Za-z]{3,}/g;
+
+function validationChecker(input, regex) {
+  //comment console in/out to use to check true and false of function
+  //console.log(regex.test(input))
+
+  //declaration for result
+  let result = document.getElementById("result");
+}
